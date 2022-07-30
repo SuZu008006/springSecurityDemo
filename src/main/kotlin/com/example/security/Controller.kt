23f71e49b0d2aae6a7cd21ef91b5a64f1baed5f9
Ryosuke Ito <suzusuke0806@gmail.com>
2022-07-30 20:22:19 +0900

    add api "Hello world!"

--- src/main/kotlin/com/example/security/Controller.kt
+++ src/main/kotlin/com/example/security/Controller.kt
@@ -1,12 +1,25 @@
 package com.example.security
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class SampleController {
     @RequestMapping("/")
     fun index(): String {
         return "index"
+    }
+}
+
+@RestController
+@RequestMapping("/api/greeting")
+@CrossOrigin(origins = ["http://localhost:3000", "https://menuplanner-web.herokuapp.com"])
+class Controller {
+    @GetMapping
+    fun getMessage(): List<String> {
+        return listOf("hello")
     }
 }
